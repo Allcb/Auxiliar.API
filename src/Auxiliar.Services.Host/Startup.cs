8baf6f7b9d61b8b054b8b6f7d4456e6acc5da971
allan.carvalho <allan.carvalho@softwillians.com.br>
2023-09-14 19:15:52 -0300

    Alteração classe program e inicio estruturação projeto

---
+++ src/Auxiliar.Services.Host/Startup.cs
@@ -0,0 +1,55 @@
+﻿namespace Auxiliar.Services.API
+{
+    public class Startup
+    {
+        #region Constantes Publicas
+
+        public const string ELMAH_BASE_PATH = "/log-errors";
+
+        #endregion Constantes Publicas
+
+        #region Propriedades Publicas
+
+        public IConfiguration Configuration { get; }
+
+        #endregion Propriedades Publicas
+
+        #region Construtores Publicos
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        #endregion Construtores Publicos
+
+        #region Metodos Publicos
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+
+        #endregion Metodos Publicos
+    }
+}
