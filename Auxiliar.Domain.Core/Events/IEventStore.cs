814fca0eb24996f5e6247da71643cc3ff2f0f179
allan.carvalho <allan.carvalho@softwillians.com.br>
2023-10-13 11:39:41 -0300

    Inicio configuração evento e mensageria, adicionado API controller.

---
+++ Auxiliar.Domain.Core/Events/IEventStore.cs
@@ -0,0 +1,11 @@
+﻿namespace Auxiliar.Domain.Core.Events
+{
+    public interface IEventStore
+    {
+        #region Metodos Publicos
+
+        void Save<T>(T @event, Guid? usuarioId = null) where T : Event;
+
+        #endregion Metodos Publicos
+    }
+}
